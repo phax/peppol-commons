c2afdd314e9888467e9026e5d54e9958ccca9360
Philip Helger <philip@helger.com>
2016-07-28 14:21:51 +0200

    Fixed docu typo

--- peppol-commons/src/main/java/com/helger/peppol/bdxr/BDXRExtensionConverter.java
+++ peppol-commons/src/main/java/com/helger/peppol/bdxr/BDXRExtensionConverter.java
@@ -134,7 +134,7 @@ public final class BDXRExtensionConverter
       if (aJson == null || !aJson.isArray ())
         throw new IllegalArgumentException ("Error in parsing extension JSON '" + sJson + "'");
 
-      final ICommonsList <ExtensionType> ret = new CommonsArrayList <> ();
+      final ICommonsList <ExtensionType> ret = new CommonsArrayList<> ();
       aJson.getAsArray ().forEach (aChild -> {
         final IJsonObject aObject = aChild.getAsObject ();
         final ExtensionType aExt = new ExtensionType ();
@@ -172,7 +172,7 @@ public final class BDXRExtensionConverter
     }
     catch (final IllegalArgumentException ex)
     {
-      // Invalid XML passed
+      // Invalid JSON passed
       return null;
     }
   }
