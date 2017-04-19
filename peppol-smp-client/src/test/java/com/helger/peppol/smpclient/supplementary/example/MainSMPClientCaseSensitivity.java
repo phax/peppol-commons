775eeae624cfa308cd823b8b20b37e7141fe2a2a
Philip Helger <philip@helger.com>
2017-04-19 14:31:01 +0200

    Renamed class; #11

---
+++ peppol-smp-client/src/test/java/com/helger/peppol/smpclient/supplementary/example/MainSMPClientCaseSensitivity.java
@@ -0,0 +1,54 @@
+package com.helger.peppol.smpclient.supplementary.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.helger.peppol.identifier.factory.PeppolIdentifierFactory;
+import com.helger.peppol.identifier.generic.doctype.IDocumentTypeIdentifier;
+import com.helger.peppol.identifier.generic.participant.IParticipantIdentifier;
+import com.helger.peppol.identifier.generic.process.IProcessIdentifier;
+import com.helger.peppol.sml.ESML;
+import com.helger.peppol.smp.ESMPTransportProfile;
+import com.helger.peppol.smp.EndpointType;
+import com.helger.peppol.smpclient.SMPClientReadOnly;
+import com.helger.peppol.url.PeppolURLProvider;
+
+public final class MainSMPClientCaseSensitivity
+{
+  private static final Logger s_aLogger = LoggerFactory.getLogger (MainSMPClientCaseSensitivity.class);
+
+  public static void main (final String [] args) throws Exception
+  {
+    final IParticipantIdentifier participantId = PeppolIdentifierFactory.INSTANCE.createParticipantIdentifierWithDefaultScheme ("0088:5060510050006");
+    s_aLogger.info ("Participant: " + participantId.getURIEncoded ());
+    s_aLogger.info ("Participant ID case INsensitive: " +
+                    PeppolIdentifierFactory.INSTANCE.isParticipantIdentifierCaseInsensitive (participantId.getScheme ()));
+
+    final SMPClientReadOnly smpClient = new SMPClientReadOnly (PeppolURLProvider.INSTANCE,
+                                                               participantId,
+                                                               ESML.DIGIT_PRODUCTION);
+
+    final IDocumentTypeIdentifier doctypeId = PeppolIdentifierFactory.INSTANCE.createDocumentTypeIdentifierWithDefaultScheme ("urn:oasis:names:specification:ubl:schema:xsd:Order-2::Order##urn:www.cenbii.eu:transaction:biitrns001:ver2.0:extended:urn:www.peppol.eu:bis:peppol28a:ver1.0::2.1");
+    s_aLogger.info ("DocType: " + doctypeId.getURIEncoded ());
+    s_aLogger.info ("DocType ID case INsensitive: " +
+                    PeppolIdentifierFactory.INSTANCE.isDocumentTypeIdentifierCaseInsensitive (doctypeId.getScheme ()));
+    IProcessIdentifier processId = PeppolIdentifierFactory.INSTANCE.createProcessIdentifierWithDefaultScheme ("URN:WWW.CENBII.EU:PROFILE:BII28:VER2.0");
+    s_aLogger.info ("Process[1]: " + processId.getURIEncoded ());
+    s_aLogger.info ("Process ID case INsensitive: " +
+                    PeppolIdentifierFactory.INSTANCE.isProcessIdentifierCaseInsensitive (processId.getScheme ()));
+
+    EndpointType endpoint = smpClient.getEndpoint (participantId,
+                                                   doctypeId,
+                                                   processId,
+                                                   ESMPTransportProfile.TRANSPORT_PROFILE_AS2);
+    s_aLogger.info ("1 - " + endpoint);
+
+    // Won't work, because process identifiers in PEPPOL are case sensitive!
+    processId = PeppolIdentifierFactory.INSTANCE.createProcessIdentifierWithDefaultScheme ("urn:www.cenbii.eu:profile:bii28:ver2.0");
+    s_aLogger.info ("Process[2]: " + processId.getURIEncoded ());
+    s_aLogger.info ("Process ID case INsensitive: " +
+                    PeppolIdentifierFactory.INSTANCE.isProcessIdentifierCaseInsensitive (processId.getScheme ()));
+    endpoint = smpClient.getEndpoint (participantId, doctypeId, processId, ESMPTransportProfile.TRANSPORT_PROFILE_AS2);
+    s_aLogger.info ("2 - " + endpoint);
+  }
+}
