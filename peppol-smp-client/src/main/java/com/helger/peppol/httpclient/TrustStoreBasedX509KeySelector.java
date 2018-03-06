be31930b428e3d7ff9cfb44d4c4e556e3b7ca845
Philip Helger <philip@helger.com>
2018-03-06 18:19:23 +0100

    Correct constant

--- peppol-smp-client/src/main/java/com/helger/peppol/httpclient/TrustStoreBasedX509KeySelector.java
+++ peppol-smp-client/src/main/java/com/helger/peppol/httpclient/TrustStoreBasedX509KeySelector.java
@@ -26,7 +26,6 @@ import javax.xml.crypto.KeySelectorException;
 import javax.xml.crypto.KeySelectorResult;
 import javax.xml.crypto.XMLCryptoContext;
 import javax.xml.crypto.XMLStructure;
-import javax.xml.crypto.dsig.DigestMethod;
 import javax.xml.crypto.dsig.SignatureMethod;
 import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 import javax.xml.crypto.dsig.keyinfo.X509Data;
@@ -82,8 +81,8 @@ public final class TrustStoreBasedX509KeySelector extends KeySelector
       if (sAlgName.equalsIgnoreCase ("RSA"))
       {
         if (sAlgURI.equalsIgnoreCase (SignatureMethod.RSA_SHA1) ||
-            sAlgURI.equalsIgnoreCase (DigestMethod.SHA256) ||
-            sAlgURI.equalsIgnoreCase (DigestMethod.SHA512))
+            sAlgURI.equalsIgnoreCase ("http://www.w3.org/2001/04/xmldsig-more#rsa-sha256") ||
+            sAlgURI.equalsIgnoreCase ("http://www.w3.org/2001/04/xmldsig-more#rsa-sha512"))
           return true;
       }
       else
