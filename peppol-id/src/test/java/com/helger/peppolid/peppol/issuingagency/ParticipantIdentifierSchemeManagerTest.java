99d01c674e4288c60f74f466073c59110c38de9c
Philip Helger <philip@helger.com>
2019-10-25 13:49:57 +0200

    Test fix

--- peppol-id/src/test/java/com/helger/peppolid/peppol/issuingagency/ParticipantIdentifierSchemeManagerTest.java
+++ peppol-id/src/test/java/com/helger/peppolid/peppol/issuingagency/ParticipantIdentifierSchemeManagerTest.java
@@ -32,7 +32,7 @@ public final class ParticipantIdentifierSchemeManagerTest
   public void testAll ()
   {
     assertNotNull (ParticipantIdentifierSchemeManager.getAllSchemes ());
-    assertEquals (82, ParticipantIdentifierSchemeManager.getAllSchemes ().size ());
+    assertEquals (80, ParticipantIdentifierSchemeManager.getAllSchemes ().size ());
 
     // test valid
     assertNotNull (ParticipantIdentifierSchemeManager.getSchemeOfISO6523Code ("0088"));
