0650b8564f12431c561f9ffbd6f5d3b42f6e2384
Philip Helger <philip@helger.com>
2020-03-02 13:32:43 +0100

    Added OCSP test with real certs

--- peppol-commons/src/test/java/com/helger/peppol/utils/PeppolCertificateCheckerTest.java
+++ peppol-commons/src/test/java/com/helger/peppol/utils/PeppolCertificateCheckerTest.java
@@ -17,13 +17,19 @@
 package com.helger.peppol.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.io.File;
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
 import java.time.Month;
 
 import org.junit.Test;
 
 import com.helger.commons.datetime.PDTFactory;
 import com.helger.commons.state.ETriState;
+import com.helger.security.keystore.EKeyStoreType;
+import com.helger.security.keystore.KeyStoreHelper;
 
 /**
  * Test class for class {@link PeppolCertificateChecker}
@@ -53,10 +59,49 @@ public class PeppolCertificateCheckerTest
                                                            ETriState.UNDEFINED);
     assertEquals (EPeppolCertificateCheckResult.EXPIRED, e);
 
+    // It's the same certificate, but we need one issued by the pilot AP
     e = PeppolCertificateChecker.checkPeppolAPCertificate (PeppolKeyStoreHelper.Config2018.CERTIFICATE_PILOT_AP,
                                                            null,
                                                            ETriState.UNDEFINED,
                                                            ETriState.UNDEFINED);
     assertEquals (EPeppolCertificateCheckResult.UNSUPPORTED_ISSUER, e);
+  }
+
+  @Test
+  public void testRealAPCert () throws Exception
+  {
+    final File fAP = new File ("src/test/resources/test-ap.p12");
+    if (fAP.exists ())
+    {
+      final KeyStore aKS = KeyStoreHelper.loadKeyStore (EKeyStoreType.PKCS12, fAP.getAbsolutePath (), "peppol")
+                                         .getKeyStore ();
+      assertNotNull (aKS);
+
+      final X509Certificate aCert = (X509Certificate) aKS.getCertificate (aKS.aliases ().nextElement ());
+      final EPeppolCertificateCheckResult e = PeppolCertificateChecker.checkPeppolAPCertificate (aCert,
+                                                                                                 null,
+                                                                                                 ETriState.FALSE,
+                                                                                                 ETriState.TRUE);
+      assertEquals (EPeppolCertificateCheckResult.VALID, e);
+    }
+  }
+
+  @Test
+  public void testRealSMPCert () throws Exception
+  {
+    final File fSMP = new File ("src/test/resources/test-smp.p12");
+    if (fSMP.exists ())
+    {
+      final KeyStore aKS = KeyStoreHelper.loadKeyStore (EKeyStoreType.PKCS12, fSMP.getAbsolutePath (), "peppol")
+                                         .getKeyStore ();
+      assertNotNull (aKS);
+
+      final X509Certificate aCert = (X509Certificate) aKS.getCertificate (aKS.aliases ().nextElement ());
+      final EPeppolCertificateCheckResult e = PeppolCertificateChecker.checkPeppolSMPCertificate (aCert,
+                                                                                                  null,
+                                                                                                  ETriState.FALSE,
+                                                                                                  ETriState.TRUE);
+      assertEquals (EPeppolCertificateCheckResult.VALID, e);
+    }
   }
 }
