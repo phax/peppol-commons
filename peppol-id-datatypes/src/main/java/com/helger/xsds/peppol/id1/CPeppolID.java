12c750ea7ff48f96d46dd1fc3e6828e0721bb1c6
Philip Helger <philip@helger.com>
2021-02-16 10:49:29 +0100

    Added new id-datatypes project

---
+++ peppol-id-datatypes/src/main/java/com/helger/xsds/peppol/id1/CPeppolID.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2015-2021 Philip Helger
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.xsds.peppol.id1;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.helger.commons.annotation.PresentForCodeCoverage;
+import com.helger.commons.io.resource.ClassPathResource;
+
+/**
+ * Constants on Peppol identifiers.
+ *
+ * @author Philip Helger
+ */
+@Immutable
+public final class CPeppolID
+{
+  /**
+   * The delimiter used between the identifier scheme and the value ("::").
+   */
+  public static final String URL_SCHEME_VALUE_SEPARATOR = "::";
+
+  public static final String NS_URI_PEPPOL_IDENTIFIERS = "http://busdox.org/transport/identifiers/1.0/";
+
+  public static final String NS_URI_PEPPOL_CODELISTS = "";
+
+  @PresentForCodeCoverage
+  private static final CPeppolID s_aInstance = new CPeppolID ();
+
+  private CPeppolID ()
+  {}
+
+  @Nonnull
+  public static ClassPathResource getXSDPeppolIdentifiers ()
+  {
+    return new ClassPathResource ("/schemas/peppol-identifiers-v1.xsd", CPeppolID.class.getClassLoader ());
+  }
+
+  @Nonnull
+  public static ClassPathResource getXSDPeppolCodeLists ()
+  {
+    return new ClassPathResource ("/schemas/peppol-codelists-v1.xsd", CPeppolID.class.getClassLoader ());
+  }
+}
