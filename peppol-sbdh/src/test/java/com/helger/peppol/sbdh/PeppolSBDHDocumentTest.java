b6c5ebad67bd2b7cc4b6fa3263f58426b26f3754
Philip Helger <philip@helger.com>
2022-01-03 19:15:39 +0100

    Updated license to 2022

--- peppol-sbdh/src/test/java/com/helger/peppol/sbdh/PeppolSBDHDocumentTest.java
+++ peppol-sbdh/src/test/java/com/helger/peppol/sbdh/PeppolSBDHDocumentTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2021 Philip Helger
+ * Copyright (C) 2014-2022 Philip Helger
  * philip[at]helger[dot]com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
