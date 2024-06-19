2f236fddd8f558aaa7b8471945df0f08349bea49
Robinson Garcia <rgarciameldz@gmail.com>
2024-06-19 11:22:26 -0600

    adding ID and CreationDateTime attributes

---
+++ dbnalliance-xhe/src/main/java/com/helger/peppol/xhe/write/DBNAllianceXHEDocumentWriter.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.helger.peppol.xhe.write;
+
+/**
+ *
+ * @author robinsongarcia
+ */
+public class DBNAllianceXHEDocumentWriter {
+  
+}
