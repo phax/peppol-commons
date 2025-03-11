e1ceb600d1bddb36f60b2e049561fbced11634e3
Philip Helger <philip@helger.com>
2025-03-11 21:02:29 +0100

    Made final

--- peppol-mlr/src/main/java/com/helger/peppol/mlr/CPeppolMLR.java
+++ peppol-mlr/src/main/java/com/helger/peppol/mlr/CPeppolMLR.java
@@ -24,7 +24,7 @@ import javax.annotation.concurrent.Immutable;
  * @author Philip Helger
  */
 @Immutable
-public class CPeppolMLR
+public final class CPeppolMLR
 {
   public static final String MLR_CUSTOMIZATION_ID = "urn:fdc:peppol.eu:poacc:trns:mlr:3";
   public static final String MLR_PROFILE_ID = "urn:fdc:peppol.eu:poacc:bis:mlr:3";
