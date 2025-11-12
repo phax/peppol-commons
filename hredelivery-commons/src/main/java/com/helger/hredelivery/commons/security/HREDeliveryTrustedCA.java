3f1113cc900682f1355133ee9a8f4917898c0806
Philip Helger <philip@helger.com>
2025-11-12 10:19:54 +0100

    Added trusted CAs

---
+++ hredelivery-commons/src/main/java/com/helger/hredelivery/commons/security/HREDeliveryTrustedCA.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2015-2025 Philip Helger
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.hredelivery.commons.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.helger.annotation.concurrent.ThreadSafe;
+import com.helger.security.certificate.TrustedCAChecker;
+
+import jakarta.annotation.Nonnull;
+
+/**
+ * This class contains all the trusted CAs in Peppol.
+ *
+ * @author Philip Helger
+ * @since 12.1.1
+ */
+@ThreadSafe
+public final class HREDeliveryTrustedCA
+{
+  private static final Logger LOGGER = LoggerFactory.getLogger (HREDeliveryTrustedCA.class);
+
+  private static final TrustedCAChecker FINA_DEMO = new TrustedCAChecker (HREDeliveryTrustStores.Fina2015.CERTIFICATE_DEMO_CA_2020);
+  private static final TrustedCAChecker FINA_PROD = new TrustedCAChecker (HREDeliveryTrustStores.Fina2015.CERTIFICATE_PRODUCTION_RDC_2020);
+
+  private HREDeliveryTrustedCA ()
+  {}
+
+  /**
+   * @return The CA checker for HR eDelivery Fina Demo.
+   */
+  @Nonnull
+  public static TrustedCAChecker hrEdeliveryFinaDemo ()
+  {
+    return FINA_DEMO;
+  }
+
+  /**
+   * @return The CA checker for HR eDelivery Fina Production.
+   */
+  @Nonnull
+  public static TrustedCAChecker hrEdeliveryFinaProduction ()
+  {
+    return FINA_PROD;
+  }
+
+  /**
+   * Remove all entries from the revocation cache.
+   */
+  public static void clearRevocationCheckCache ()
+  {
+    FINA_DEMO.getRevocationCache ().clearCache ();
+    FINA_PROD.getRevocationCache ().clearCache ();
+
+    LOGGER.info ("The HREDeliveryTrustedCA revocation cache was cleared");
+  }
+}
